0f3e06119ed52f07d487c13fa2871ff7e56a9297
Uncle Cheese <aaron@silverstripe.com>
2015-04-18 14:28:35 +1200

    source code comments

--- RoboTypePlugin.py
+++ RoboTypePlugin.py
@@ -2,30 +2,42 @@ import sublime, sublime_plugin, random, time
   
 class RoboTypeCommand(sublime_plugin.TextCommand):  
 	
+	# A basic map of the keyboard.
 	keys = [
 		['q','w','e','r','t','y','u','i','o','p','[',']'],
 		['a','s','d','f','g','h','j','k','l',';','\''],
 		['z','x','c','v','b','n','m',',','.','/']
 	]
 
+	# The fastest possible keystroke, in milliseconds
 	intervalLow = 10
 
+	# The slowest possible keystroke, in milliseconds
 	intervalHigh = 100
 
+	# The position of the first occuring typo in the current rendered text
 	typoIndex = None
 
+	# The delay, in milliseconds for each keystroke. Continually augmented.
 	timeout = None
 
+	# The index of the cliboard text currently being rendered
 	stringIndex = 0
 
 	def run(self, edit):
+		# Get text from clipboard
 		self.text_to_print = sublime.get_clipboard()		
+		
+		# Override from settings
 		self.intervalLow = self.view.settings().get('robotype_keystroke_interval_low')
 		self.intervalHigh = self.view.settings().get('robotype_keystroke_interval_high')
 
+		# Establish an initial timout
 		self.timeout = self.getInterval()
 		
+		# Loop through the string
 		while self.stringIndex < len(self.text_to_print):			
+			# If there is a typo on the screen, and the robot "notices" it, backspace.
 			if(self.typoIndex is not None):				
 				if(self.willBackspace()):					
 					self.backspaceTo(self.typoIndex)
@@ -34,6 +46,7 @@ class RoboTypeCommand(sublime_plugin.TextCommand):
 			nextChar = self.text_to_print[self.stringIndex]
 			keystroke = self.generateKeystroke(nextChar)
 			
+			# If a typo was rendered, capture its position
 			if( (keystroke is not nextChar) and (self.typoIndex is None) ):								
 				self.typoIndex = self.stringIndex
 			
@@ -42,18 +55,22 @@ class RoboTypeCommand(sublime_plugin.TextCommand):
 
 		self.reset()
 
+	# Queues the rendering of a character
 	def renderChar(self, char):
 		self.queueAction(lambda char=char: self.view.run_command('robo_type_add_char', {"args" : { 'char': char }}))
 
+	# Backspaces to a position in the string
 	def backspaceTo(self, index):
 		while self.stringIndex > index:			
 			self.queueAction(lambda: self.view.run_command('robo_type_delete_char'))
 			self.stringIndex -= 1
 
+	# Schedule an action, using the incrementing timeout
 	def queueAction(self, func):
 		self.timeout += self.getInterval()
 		sublime.set_timeout(func, self.timeout)
 
+	# Did the robot "notice" the typo?
 	def willBackspace(self):
 		if(self.stringIndex is (len(self.text_to_print)-1)):
 			return true
@@ -63,9 +80,11 @@ class RoboTypeCommand(sublime_plugin.TextCommand):
 		
 		return rand is 1
 
+	# Gets a random keystroke speed, using the low/high threshold from the settings
 	def getInterval(self):
 		return random.randrange(self.intervalLow, self.intervalHigh+1)
 
+	# Given a character, render it, or a typo, depending on the outcome
 	def generateKeystroke(self, char):
 		chance = self.view.settings().get('robotype_keystroke_accuracy')
 		rand = random.randrange(1, chance+1)
@@ -90,14 +109,14 @@ class RoboTypeCommand(sublime_plugin.TextCommand):
 		self.typoIndex = None	
 
 
-
+# Adds a character to the screen
 class RoboTypeAddCharCommand(sublime_plugin.TextCommand):
 	
 	def run(self, edit, args):
 		self.view.insert(edit, self.view.sel()[0].begin(), args['char'])
 
 
-
+# Deletes the previous character from the screen
 class RoboTypeDeleteCharCommand(sublime_plugin.TextCommand):
 	
 	def run(self, edit):
